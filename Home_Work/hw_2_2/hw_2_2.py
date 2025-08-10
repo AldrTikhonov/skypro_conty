af26db30933a5294f93ecbc66cc53cd08a8a8433
Alexander Tikhonov <AldrTikhonov>
2025-08-10 11:29:31 +0300

    add ales two

--- Home_Work/hw_2_2/hw_2_2.py
+++ Home_Work/hw_2_2/hw_2_2.py
@@ -10,32 +10,32 @@ print("Привет! Предлагаю проверить свои знания
 name = input("Напиши, как тебя зовут \n")
 print("Привет,", name, "начинаем тренировку!")
 
-print("Вопрос 1: Mi name ___ Vova.")
+print("__________\nВопрос 1: Mi name ___ Vova.")
 answer_1 = input(result_1)
 if answer_1 == correct_answer_1:
     count += 10
     count_1 += 1
-    print("Ответ верный!\nВы получаете", count, "баллов!\n____________")
+    print("Ответ верный!\nВы получаете", count, "баллов!")
 else:
     print(result_2, correct_answer_1)
 
-print("Вопрос 2: I ___ a coder.")
+print("__________\nВопрос 2: I ___ a coder.")
 answer_2 = input(result_1)
 if answer_2 == correct_answer_2:
     count += 10
     count_1 += 1
-    print("Ответ верный!\nВы получаете", count, "баллов!\n____________")
+    print("Ответ верный!\nВы получаете", count, "баллов!")
 else:
     print(result_2, correct_answer_2)
 
-print("Вопрос 3: I love ___ Moscow.")
+print("__________\nВопрос 3: I love ___ Moscow.")
 answer_3 = input(result_1)
 if answer_3 == correct_answer_3:
     count += 10
     count_1 += 1
-    print("Ответ верный!\nВы получаете", count, "баллов!\n____________")
+    print("Ответ верный!\nВы получаете", count, "баллов!")
 else:
     print(result_2, correct_answer_3)
 
-print(f"Вот и все, ", name, "!\nВы ответили на ", count_1, "вопросов из 3 верно.")
+print(f"__________\nВот и все, ", name, "!\nВы ответили на ", count_1, "вопросов из 3 верно.")
 print(f"Вы заработали ", count, "баллов.\nЭто ", round(count_1/3 * 100, 2), "процентов.")
